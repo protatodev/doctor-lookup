8a73620f44efd318db42dcffd4707053107b883a
protatodev <thaddonaghue@yahoo.com>
2018-08-10 13:56:55 -0700

    enhance styling

--- src/main.js
+++ src/main.js
@@ -53,6 +53,10 @@ function displayResults(name, issue) {
               <h4 class="card-title">${results.data[i].profile.first_name} ${results.data[i].profile.last_name}</h4>
               <h6 class="card-subtitle text-muted">${results.data[i].practices[0].visit_address.city}, ${results.data[i].practices[0].visit_address.state}</h6>
               <h6 class="card-subtitle text-muted">${results.data[i].practices[0].visit_address.street}</h6>
+              <h6 class="card-subtitle text-muted">${results.data[i].practices[0].phones[0].number}</h6>
+              <br>
+              <h5 id="available${i}" class="card-subtitle"></h5>
+              <br>
               <p class="card-text"><strong>About ${results.data[i].profile.first_name}:</strong></p>
               <p class="card-text">${results.data[i].profile.bio}</p>
               <p class="card-text"><strong>Specialties:</strong></p>
@@ -61,6 +65,14 @@ function displayResults(name, issue) {
             </div>
           </div>
           </div>`);
+
+          if(results.data[i].practices[0].accepts_new_patients) {
+            $("#available" + i).text("Accepting New Patients");
+            $("#available" + i).addClass("font-green");
+          } else {
+            $("#available" + i).text("Not Accepting New Patients")
+            $("#available" + i).addClass("font-red");
+          }
           
           for(let j = 0; j < results.data[i].specialties.length; j++) {
             $("#specialties" + i).append(
