d0e4d50f1d021404cc049a79b574e36c9d498a5a
protatodev <thaddonaghue@yahoo.com>
2018-08-10 13:09:45 -0700

    improve overall styling

--- src/main.js
+++ src/main.js
@@ -36,33 +36,38 @@ function displayResults(name, issue) {
 
   promise.then(results => {
     console.log(results.data[0]);
-    for(let i = 0; i < results.data.length; i++) {
-      $(".card-group").append(
-        `<div class="col-lg-3">
-        <div class="card">
-          <img class="card-img-top" src="${results.data[i].profile.image_url}" alt="Card image cap">
-          <div class="card-body">
-            <h4 class="card-title">${results.data[i].profile.first_name} ${results.data[i].profile.last_name}</h4>
-            <h6 class="card-subtitle text-muted">${results.data[i].practices[0].visit_address.city}, ${results.data[i].practices[0].visit_address.state}</h6>
-            <h6 class="card-subtitle text-muted">${results.data[i].practices[0].visit_address.street}</h6>
-            <p class="card-text"><strong>About ${results.data[i].profile.first_name}:</strong></p>
-            <p class="card-text">${results.data[i].profile.bio}</p>
-            <p class="card-text"><strong>Specialties:</strong></p>
-            <ul id="specialties${i}">
-            </ul>
+    if(results.data.length === 0) {
+      $(".errorDiv").hide().fadeIn(1000);
+    } else {
+      for(let i = 0; i < results.data.length; i++) {
+        $(".card-group").append(
+          `<div class="col-lg-3">
+          <div class="card text-white bg-info">
+            <img class="card-img-top" src="${results.data[i].profile.image_url}" alt="Card image cap">
+            <div class="card-body">
+              <h4 class="card-title">${results.data[i].profile.first_name} ${results.data[i].profile.last_name}</h4>
+              <h6 class="card-subtitle text-muted">${results.data[i].practices[0].visit_address.city}, ${results.data[i].practices[0].visit_address.state}</h6>
+              <h6 class="card-subtitle text-muted">${results.data[i].practices[0].visit_address.street}</h6>
+              <p class="card-text"><strong>About ${results.data[i].profile.first_name}:</strong></p>
+              <p class="card-text">${results.data[i].profile.bio}</p>
+              <p class="card-text"><strong>Specialties:</strong></p>
+              <ul id="specialties${i}">
+              </ul>
+            </div>
           </div>
-        </div>
-        </div>`);
-        
-        for(let j = 0; j < results.data[i].specialties.length; j++) {
-          $("#specialties" + i).append(
-            `<li>${results.data[i].specialties[j].name}</li>`
-          );
-        }
+          </div>`);
+          
+          for(let j = 0; j < results.data[i].specialties.length; j++) {
+            $("#specialties" + i).append(
+              `<li>${results.data[i].specialties[j].name}</li>`
+            );
+          }
+      }
     }
     scrollToView();
   }).catch(error => {
-    $(".resultsDiv").text("Search returned 0 results!");
+    $(".errorDiv").text(error);
+    $(".errorDiv").hide().fadeIn(1000);
   });
 }
 
